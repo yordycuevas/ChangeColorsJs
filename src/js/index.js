b8b14dda1baf3b19609e9e75389256530e013ba3
yordycuevas <yordycuevas21@hotmail.com>
2022-02-21 18:05:04 -0500

    changeColors

---
+++ src/js/index.js
@@ -0,0 +1,19 @@
+const colors = ['green', 'yellow', 'rgba(133,122,200)', '#f15025'];
+
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+btn.addEventListener('click', () =>{
+
+    // get randomNumber between 0 and 3
+    const randomNumber = getRandomNumber();
+    console.log(randomNumber);
+
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+});
+
+function getRandomNumber (){
+    return Math.floor (Math.random()*colors.length);
+};
+
